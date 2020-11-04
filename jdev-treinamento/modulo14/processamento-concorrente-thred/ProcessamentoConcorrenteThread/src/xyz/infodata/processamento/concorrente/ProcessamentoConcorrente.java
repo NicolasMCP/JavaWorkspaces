401bfafa4b80eac5df0549dcfc461b3e86866bb9
NicolasMCP <nicolas.mcp@gmail.com>
2020-11-03 23:03:42 -0300

    Mód.14 duas thread

---
+++ jdev-treinamento/modulo14/processamento-concorrente-thred/ProcessamentoConcorrenteThread/src/xyz/infodata/processamento/concorrente/ProcessamentoConcorrente.java
@@ -0,0 +1,19 @@
+package xyz.infodata.processamento.concorrente;
+
+/**
+ * @author Nicolas Ramos, nicolas@infodata.xyz
+ * @version 0.1.0
+ * @created 03/11/2020 
+ * @apiNote https://cursocompletoemjsfcomprimeface.club.hotmart.com/lesson/9M7GkjgP7w/processamento-concorrente-entre-duas-thread
+ *          este código DIFERE do código da aula. ;)
+ *                     --------
+ **/
+
+public class ProcessamentoConcorrente {
+
+	public static void main(String[] args) {
+		
+		System.out.println("somente para teste no github...");
+	}
+
+}
