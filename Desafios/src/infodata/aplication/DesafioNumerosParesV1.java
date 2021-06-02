d866abc4b62373cea473bf7c5309b29748c9c514
NicolasMCP <nicolas.mcp@gmail.com>
2021-06-02 00:13:48 -0300

    Desafio Numeros Pares

---
+++ Desafios/src/infodata/aplication/DesafioNumerosParesV1.java
@@ -0,0 +1,27 @@
+package infodata.aplication;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Imprimir em Console os Números pares até o valor fornecido (inclusive se for o caso).
+ * Use BufferedReader para ler o teclado.
+ *
+ * Nota: Utilize System.out.println() apenas para os resultados de saída
+ *
+ *
+ * @author Nicolas Ramos
+ * email: nicolas.mcp@gmail.com
+ *
+ */
+
+public class DesafioNumerosParesV1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int entrada = Integer.parseInt(br.readLine());
+
+        for (int i = 1 ; i <= entrada ; i++) {
+            if (i % 2 == 0 ) System.out.println(i);
+        }
+    }
+}
